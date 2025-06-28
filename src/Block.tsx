bb9941d075185368aa3023b9d0510f1385e94eaa
Gavin Morrow <gavinfmorrow@gmail.com>
2025-06-28 00:04:58 -0400

    change params

--- src/Block.tsx
+++ src/Block.tsx
@@ -2,6 +2,31 @@ import { DragEventHandler, useState } from 'react';
 import './Block.css';
 import { stmtToString, Stmt } from './Program';
 
+const updateProp = (prop, setProp, setValue) => (newValue) => {
+  const newProp = structuredClone(prop);
+  setValue(newProp, newValue);
+  setProp(newProp);
+};
+
+const ParamInput = ({ value, setValue }) => {
+  const [tmpVal, setTmpVal] = useState(value);
+  const commitChange = () => {
+    let newValue = Number(setTmpVal);
+    if (isNaN(newValue)) newValue = value;
+
+    setTmpVal(newValue);
+    setValue(newValue);
+  };
+  return (
+    <input
+      type="number"
+      value={tmpVal}
+      onChange={(e) => setTmpVal(e.target.value)}
+      onBlur={commitChange}
+    />
+  );
+};
+
 type BlockProps = {
   stmt: Stmt | { type: 'hole' };
   setStmt: (stmt: Stmt) => void;
@@ -18,13 +43,14 @@ const Block = ({ stmt, setStmt, delStmt }: BlockProps) => {
       'application/logo-blocks.stmt',
       JSON.stringify(stmt),
     );
-    event.dataTransfer.setData('application/json', JSON.stringify(stmt));
-    event.dataTransfer.setData('text/plain', stmtToString(stmt));
+    // Don't do this b/c of dropping into inputs, which is very annoying
+    // event.dataTransfer.setData('application/json', JSON.stringify(stmt));
+    // event.dataTransfer.setData('text/plain', stmtToString(stmt));
 
     event.stopPropagation();
   };
   const onDragEnd: DragEventHandler<HTMLDivElement> = (event) => {
-    if (event.dataTransfer.dropEffect != 'none') {
+    if (event.dataTransfer.dropEffect == 'move') {
       // The drop wasn't completed.
       delStmt();
     }
@@ -85,17 +111,48 @@ const Block = ({ stmt, setStmt, delStmt }: BlockProps) => {
         </>
       );
       break;
-    case 'command0':
+    case 'command0': {
       value = stmt.name;
       break;
-    case 'command1':
-      value = `${stmt.name} ${stmt.value}`;
+    }
+    case 'command1': {
+      const [tmpVal, setTmpVal] = useState(String(stmt.value));
+      const setVal = (newVal: number) => {
+        setTmpVal(String(newVal));
+        const newStmt = structuredClone(stmt);
+        newStmt.value = newVal;
+        setStmt(newStmt);
+      };
+      value = (
+        <>
+          {stmt.name}{' '}
+          <input
+            type="number"
+            value={tmpVal}
+            onChange={(e) => setTmpVal(e.target.value)}
+            onBlur={(event) => setVal(Number(event.target.value))}
+            style={{ width: '5ch', border: 'none', fontFamily: 'monospace' }}
+          />
+        </>
+      );
       break;
-    case 'repeat':
+    }
+    case 'repeat': {
+      const setCount = (newCount: number) => {
+        const newStmt = structuredClone(stmt);
+        newStmt.count = newCount;
+        setStmt(newStmt);
+      };
       value = (
         <>
           {' '}
-          {'repeat'} {stmt.count}{' '}
+          {'repeat'}{' '}
+          <input
+            type="number"
+            value={stmt.count}
+            onSubmit={(event) => setCount(Number(event.target.value))}
+            style={{ width: '5ch', border: 'none', fontFamily: 'monospace' }}
+          />
           <Block
             stmt={stmt.stmts}
             setStmt={(newS) => {
@@ -117,6 +174,7 @@ const Block = ({ stmt, setStmt, delStmt }: BlockProps) => {
         </>
       );
       break;
+    }
     case 'func':
     case 'call':
       value = 'unknown';
