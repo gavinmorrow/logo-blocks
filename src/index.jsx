15daf65c0fef0a9c67a4b9eeb0a97e503eb41136
Gavin Morrow <gavinfmorrow@gmail.com>
2025-07-01 17:37:23 -0400

    Add tab title

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <title>Logo Blocks</title>
     <App />
   </React.StrictMode>,
 );
