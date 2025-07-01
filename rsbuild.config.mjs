8ef4a6c658201e787069c2fe5fba5a173b1d12c0
Gavin Morrow <gavinfmorrow@gmail.com>
2025-07-01 17:35:01 -0400

    Fix paths on github pages

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -3,4 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    // Make it work on github pages
+    // See <https://stackoverflow.com/questions/78750417/how-do-i-force-rspack-rsbuild-to-use-relative-paths>
+    assetPrefix: './',
+  },
 });
