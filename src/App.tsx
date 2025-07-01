0fd60b03fca9e22f70c46ae4b451f9a439a4f596
Gavin Morrow <gavinfmorrow@gmail.com>
2025-06-30 23:43:26 -0400

    Delay and no removing outer block

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import Editor from './Editor';
 import './App.css';
-import { FLOWER, HELLO_WORLD, stmtToString } from './Program';
+import { FLOWER, HELLO_WORLD, Program, stmtToString } from './Program';
 import run from './run';
 import { useState } from 'react';
 
 const App = () => {
-  const [program, setProgram] = useState(HELLO_WORLD);
+  const [delay, setDelay] = useState(100);
+
+  const [program, setProgramRaw] = useState(FLOWER);
+  const setProgram = (program: Program) => {
+    if (program.type != 'block') {
+      program = { type: 'block', stmts: [program] };
+    }
+    setProgramRaw(program);
+  };
 
   return (
     <>
       <button
         onClick={() =>
-          run(program, { pd: true, x: 250, y: 250, r: 0 }, 100, true)
+          run(program, { pd: true, x: 250, y: 250, r: 0 }, delay, true)
         }
       >
         Run
@@ -28,6 +36,11 @@ const App = () => {
       >
         Import
       </button>
+      <input
+        type="number"
+        value={delay}
+        onChange={(e) => setDelay(Number(e.target.value))}
+      />
       <Editor {...{ program, setProgram }} />
       <canvas id="canvas" width={500} height={500} />
     </>
